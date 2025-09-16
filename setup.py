df07d33888b294d69089f531dd70fb27960227ce
harsh <jainharsh5903@gmail.com>
2025-09-16 03:16:31 -0500

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="My_Medical_Chatbot",
+    version="0.1.0",
+    author="Harsh Shripal Jain",
+    author_email="harshjain7872@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
